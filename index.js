c54ff6b8348d7ab0ba4559cd96adc018cb18e9a2
Matybsas <126203934+Matybsas@users.noreply.github.com>
2023-04-17 22:02:35 -0300

    Responsive index,creacion neumaticos para appi

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ function mensajeEnviado(){
 let envioMensaje= (`Hola ${nombre} ${apellido} , Tu Consulta fue enviado Exitosamente, a la brevedad nos pondremos en contacto.`);
 document.write(envioMensaje);
 }
+
+//BOTON DE SCROLL-UP
+$(document).ready(function(){
+
+	$('.ir-inicio').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.ir-inicio').slideDown(300);
+		} else {
+			$('.ir-inicio').slideUp(300);
+		}
+	});
+
+});
